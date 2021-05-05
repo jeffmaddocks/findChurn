2e92fe4439b1fd1d070ff570d9553bab66f6a800
jeffmaddocks <madjeff@gmail.com>
2021-05-05 11:28:23 -0500

    cleaned up data prep

--- dataprep.py
+++ dataprep.py
@@ -7,10 +7,10 @@ kaggle.api.dataset_download_files('blastchar/telco-customer-churn', unzip=True) 
 
 df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
 
-historical_outcomes=df.sample(frac=0.5,random_state=200) #random state is a seed value
-current_customers=df.drop(historical_outcomes.index)
-
-historical_outcomes.drop(columns='Churn')
+historical_outcomes=df.sample(frac=0.6,random_state=200) #random state is a seed value
+current_customers = current_customers=df.drop(historical_outcomes.index)
+current_customers = current_customers.drop(columns='Churn')
 
 historical_outcomes.to_json('Telco_Churn_historical_outcomes.json')
 current_customers.to_json('Telco_Churn_current_customers.json')
+print()
